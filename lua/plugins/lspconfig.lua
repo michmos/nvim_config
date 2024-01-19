186f4d97a1aa98b3a51cee0d2ba04ddf275d4ba5
Michael Moser <mmoser@f1r1s19.codam.nl>
2024-01-19 09:42:39 +0100

    disabled autoformatting in general. 42header. hovering function enabled <leader>i

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,8 +4,13 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable keymap because K is used for 5k
+      --   disable keymap because K is used for 5k
       keys[#keys + 1] = { "K", false }
+      --   <leader>i can be used for hovering, instead of K
+      keys[#keys + 1] = { "<leader>i", vim.lsp.buf.hover, desc = "Hover" }
     end,
+	opts = {
+			autoformat = false,
+		},
   },
 }
